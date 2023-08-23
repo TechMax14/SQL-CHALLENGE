84901bd62b6ef24419ae4c1612aebb944b871093
Max <maxfishman@comcast.net>
2023-08-23 17:06:16 -0400

    Update tables.sql

--- EmployeeSQL/Submitting/tables.sql
+++ EmployeeSQL/Submitting/tables.sql
@@ -1,8 +1,8 @@
 ------ Creating the tables ------
 
 CREATE TABLE "title" (
-    "title_id" varchar   NOT NULL,
-    "title" varchar   NOT NULL,
+    "title_id" varchar(10)   NOT NULL,
+    "title" varchar(30)   NOT NULL,
     CONSTRAINT "pk_title" PRIMARY KEY (
         "title_id"
     )
@@ -14,8 +14,8 @@ CREATE TABLE "salaries" (
 );
 
 CREATE TABLE "departments" (
-    "dept_no" varchar   NOT NULL,
-    "dept_name" varchar   NOT NULL,
+    "dept_no" varchar(10)   NOT NULL,
+    "dept_name" varchar(30)   NOT NULL,
     CONSTRAINT "pk_departments" PRIMARY KEY (
         "dept_no"
     )
@@ -23,10 +23,10 @@ CREATE TABLE "departments" (
 
 CREATE TABLE "employees" (
     "emp_no" int   NOT NULL,
-    "emp_title_id" varchar   NOT NULL,
+    "emp_title_id" varchar(10)   NOT NULL,
     "birth_date" date   NOT NULL,
-    "first_name" varchar   NOT NULL,
-    "last_name" varchar  NOT NULL,
+    "first_name" varchar(50)   NOT NULL,
+    "last_name" varchar(50)  NOT NULL,
     "sex" char   NOT NULL,
     "hire_date" date   NOT NULL,
     CONSTRAINT "pk_employees" PRIMARY KEY (
@@ -36,7 +36,7 @@ CREATE TABLE "employees" (
 
 CREATE TABLE "dept_emp" (
     "emp_no" int   NOT NULL,
-    "dept_no" varchar   NOT NULL,
+    "dept_no" varchar(10)   NOT NULL,
     CONSTRAINT "fk_dept_emp_emp_no" FOREIGN KEY ("emp_no")
         REFERENCES "employees" ("emp_no"),
     CONSTRAINT "fk_dept_emp_dept_no" FOREIGN KEY ("dept_no")
@@ -44,7 +44,7 @@ CREATE TABLE "dept_emp" (
 );
 
 CREATE TABLE "dept_manager" (
-    "dept_no" varchar   NOT NULL,
+    "dept_no" varchar(10)   NOT NULL,
     "emp_no" int   NOT NULL,
     CONSTRAINT "fk_dept_manager_pk" PRIMARY KEY (
         "dept_no", "emp_no"
